48dcfa465b610d23a614927b10bedb42e9da5861
orlov <orlov@irkoms.ru>
2022-03-03 09:38:23 +0800

    ...

--- schema.sql
+++ schema.sql
@@ -1,26 +1,27 @@
+DROP TABLE `ads`;
+
 CREATE TABLE `ads`
 (
-    `id`             INT(10)        NOT NULL AUTO_INCREMENT,
-    -- `category_id` INT(10)        NULL,
-    -- location
+    `id`             INT            NOT NULL AUTO_INCREMENT,
+    `category_id`    INT            NOT NULL DEFAULT 0,
     `title`          VARCHAR(100)   NOT NULL,
-    `description`    TEXT           NOT NULL,
-    `www`            VARCHAR(500)   NULL,
-    `youtube`        VARCHAR(100)   NULL,
-
     `price`          DECIMAL(10, 2) NOT NULL,
     `property_type`  VARCHAR(10)    NULL, -- flat / house / other / garage / parking
-    `num_beds`       TINYINT        NULL, --  0-20
+    `num_beds`       INT            NULL, --  0-20
     `price_freq`     VARCHAR(10)    NULL, -- per_month / per_week
     `date_avail`     DATE           NULL,
     `room_type`      VARCHAR(10)    NULL, -- single / double / twin / triple / shared / couch
-    `room_couples`   TINYINT        NULL,
-
+    `room_couples`   INT            NULL,
+    `youtube`        VARCHAR(100)   NULL,
+    `www`            VARCHAR(500)   NULL,
+    `description`    TEXT           NULL,
+    `location`       VARCHAR(100)   NULL, -- ??
+    `created_at`     DATETIME       NOT NULL DEFAULT NOW(),
+    `deleted_at`     DATETIME       NULL,
     `urgent_till`    DATETIME       NULL,
     `featured_till`  DATETIME       NULL,
     `spotlight_till` DATETIME       NULL,
-    `created_at`     DATETIME       NOT NULL,
-    `deleted_at`     DATETIME       NULL,
+
     PRIMARY KEY (`id`) USING BTREE
 ) COLLATE = 'utf8_general_ci'
   ENGINE = MyISAM;
