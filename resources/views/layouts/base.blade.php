75371b4330f23d1264eebd6d9fec6db23f48401e
orlov <orlov@irkoms.ru>
2022-03-03 12:24:21 +0800

    ...

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -8,11 +8,9 @@
     <link rel="icon" href="/favicon.ico">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
           integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <script src="https://kit.fontawesome.com/e0449c5598.js" crossorigin="anonymous"></script>
     @yield('inline_styles')
 </head>
-<body class="bg-li1ght">
-
+<body>
 <nav class="navbar navbar-expand-lg navbar-light bg-light shadow-sm">
     <div class="container">
         <a class="navbar-brand" href="/">ADZ</a>
@@ -54,6 +52,7 @@
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
         crossorigin="anonymous"></script>
+<script src="https://kit.fontawesome.com/e0449c5598.js" crossorigin="anonymous"></script>
 @yield('inline_scripts')
 </body>
 </html>
