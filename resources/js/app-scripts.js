84624c2d66814082a4a9a29dbeba9aa2e57f6b71
orlov <yuraigle@gmail.com>
2022-03-12 11:00:02 +0800

    ..

--- resources/js/app-scripts.js
+++ resources/js/app-scripts.js
@@ -1,7 +1,2 @@
+import Collapse from "bootstrap/js/dist/collapse";
 import "../css/app-styles.css";
-
-document
-    .getElementsByClassName("navbar-toggler")[0]
-    .addEventListener("click", function () {
-        document.getElementById("navbar1").classList.toggle("show");
-    });
