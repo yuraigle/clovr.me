f3d0584366278120720290759542728b8d113384
orlov <orlov@irkoms.ru>
2022-04-22 09:50:04 +0800

    ...

--- speed-up.sh
+++ speed-up.sh
@@ -1,7 +1,7 @@
 composer update --no-ansi &&
 composer install --optimize-autoloader --no-dev --no-ansi &&
-php artisan cache:clear &&
-php artisan config:cache &&
-php artisan route:cache &&
-php artisan view:cache &&
+php artisan optimize:clear --no-ansi &&
+php artisan config:cache --no-ansi &&
+php artisan route:cache --no-ansi &&
+php artisan view:cache --no-ansi &&
 chown -R orlov:www-data ./storage/framework/views/
