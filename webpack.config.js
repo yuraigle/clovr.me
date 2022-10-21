cebb76f030e84320a83da915909f28442980ec98
yuraigle <yuraigle@gmail.com>
2022-10-22 07:15:23 +0800

    css purged

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,13 @@
 const path = require("path");
+const glob = require("glob");
 const webpack = require("webpack");
 const {VueLoaderPlugin} = require("vue-loader");
 const {WebpackManifestPlugin} = require("webpack-manifest-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const {PurgeCSSPlugin} = require('purgecss-webpack-plugin');
 
 const devMode = process.env.NODE_ENV !== "production";
+const dirNix = path.join(__dirname).replaceAll(/\\/g, '/');
 
 module.exports = {
     mode: devMode ? "development" : "production",
@@ -81,6 +84,10 @@ module.exports = {
             basePath: "/dist/",
             publicPath: "/dist/",
             fileName: path.resolve(__dirname, "./public/mix-manifest.json"),
+        }),
+        new PurgeCSSPlugin({
+            paths: glob.sync(`${dirNix}/resources/**/*`, { nodir: true }),
+            safelist: [/^bg-[a-z]+$/]
         }),
     ],
 };
