f0966023ad86ea6897441e83695727d8154b1113
orlov <orlov@irkoms.ru>
2023-05-15 13:16:35 +0800

    srcset fix

--- resources/views/_partials/ad_main_pic.blade.php
+++ resources/views/_partials/ad_main_pic.blade.php
@@ -1,5 +1,4 @@
 <picture>
-    <source srcset="{{ App\Helpers\AdPic::main($ad, '400') }}" type="image/webp" media="(min-width: 769px)">
-    <source srcset="{{ App\Helpers\AdPic::main($ad, '200') }}" type="image/webp" media="(max-width: 768px)">
+    <source srcset="{{ App\Helpers\AdPic::main($ad, '400') }} 2x, {{ App\Helpers\AdPic::main($ad, '200') }} 1x" type="image/webp">
     <img src="{{ App\Helpers\AdPic::main($ad, '200', 'jpg') }}" alt="pic" loading="lazy" width="200" height="150">
 </picture>
