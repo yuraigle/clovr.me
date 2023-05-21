db1ca6f6c4fa6d1b48f9a6fccc29cb0d2a1f0a9b
yuraigle <yuraigle@gmail.com>
2023-05-21 23:27:20 +0800

    ..

--- resources/views/_partials/filter.blade.php
+++ resources/views/_partials/filter.blade.php
@@ -1,12 +1,12 @@
 <h4 class="border-bottom">Filters</h4>
 
 <div class="row pt-2">
-    <label class="form-label" for="filter_price_to">Rent (per week)</label>
-    <div class="col-6">
-        <input class="form-control" name="filter[price_from]" id="filter_price_from" placeholder="0" />
-    </div>
-    <div class="col-6">
-        <input class="form-control" name="filter[price_to]" id="filter_price_to" />
+    <div class="col-12">
+        <ul>
+            <li><a href="#" class="text-decoration-none text-dark fw-bold">To Rent</a></li>
+            <li><a href="#" class="text-decoration-none">For Sale</a></li>
+            <li><a href="#" class="text-decoration-none">To Share</a></li>
+        </ul>
     </div>
 </div>
 
@@ -14,14 +14,25 @@
     <label class="form-label">Property type</label>
     <div class="col-12">
         <ul>
+{{--            <li><a href="#" class="text-decoration-none text-dark fw-bold">Any</a></li>--}}
             <li><a href="#" class="text-decoration-none">House</a></li>
-            <li><a href="#" class="text-decoration-none text-dark fw-bold">Apartment</a></li>
+            <li><a href="#" class="text-decoration-none">Apartment</a></li>
             <li><a href="#" class="text-decoration-none">Other</a></li>
         </ul>
     </div>
 </div>
 
 <div class="row pt-2">
+    <label class="form-label" for="filter_price_to">Rent (per week)</label>
+    <div class="col-6">
+        <input class="form-control" name="filter[price_from]" id="filter_price_from" placeholder="From" />
+    </div>
+    <div class="col-6">
+        <input class="form-control" name="filter[price_to]" id="filter_price_to" placeholder="To" />
+    </div>
+</div>
+
+<div class="row pt-4">
     <div class="col-12">
         <button class="btn btn-success bg-gradient w-100">
             Apply
